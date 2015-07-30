40d86368e645486d57ef39a74e03a9770051f0d7
M.Yokota <mitsuru.yokota@gmail.com>
2015-07-30 23:03:00 +0900

    java勉強プロジェクト

---
+++ src/cscjava/Mutter.java
@@ -0,0 +1,15 @@
+package cscjava;
+
+import java.io.Serializable;
+
+public class Mutter implements Serializable{
+	private String userName;	//ユーザー名
+	private String text;		//つぶやき内容
+	public Mutter(){}
+	public Mutter(String userName, String text){
+		this.userName = userName;
+		this.text = text;
+	}
+	public String getUserName(){ return userName; }
+	public String getText(){ return text; }
+}
